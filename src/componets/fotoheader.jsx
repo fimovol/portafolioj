b55bc1d7dfb962f4124dc5ae0556934b2b62dc0b
fimovol <fimovolpe@gmail.com>
2021-12-19 14:13:23 -0500

    ok

--- src/componets/fotoheader.jsx
+++ src/componets/fotoheader.jsx
@@ -1,5 +1,8 @@
 export default function _(){
+    const style={
+        width: '100%'
+    }
     return(
-        <img src='https://static-cdn.jtvnw.net/jtv_user_pictures/8d62f29a-375c-4ece-beb1-3567630d5d51-profile_image-300x300.png'/>
+        <img style={style} src='https://static-cdn.jtvnw.net/jtv_user_pictures/8d62f29a-375c-4ece-beb1-3567630d5d51-profile_image-300x300.png'/>
     )
 }
