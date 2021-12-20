9acb18baf37234a436adc8bd2c5f0eac1aeac539
fimovol <fimovolpe@gmail.com>
2021-12-20 12:42:07 -0500

    agregando un nuevo boton

--- src/componets/boton.jsx
+++ src/componets/boton.jsx
@@ -13,3 +13,10 @@ export function Botondos(){
             </button>
     )
 }
+export function Boton({nombre}){
+    return(
+        <button className="font-mono place-content-center text-xs bg-transparent hover:bg-primario1 text-secundario1 font-semibold hover:text-primario2 py-2 px-4 border border-secundario1 hover:border-transparent rounded"> 
+            {nombre}
+        </button>
+    )
+}
--- src/componets/lateral.jsx
+++ src/componets/lateral.jsx
@@ -1,27 +1,24 @@
-import { Button } from "@material-ui/core";
-import { purple, red } from '@mui/material/colors';
+import {Boton} from './boton'
 
 export default function _(){
-    const primary = red[500];
 
-    const myStyle = {
-        display: 'flex',
-        'align-items': 'center',
-        'justify-content': 'center',
-        'flex-direction': 'column',
-        color: '#ffffff',
-        backgroundColor: '#000f00',
-        height: '100%',
-        width: '100%',
-    };
     return(
-        <div style={myStyle}>
-            <h1 class="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <div>aqui unos puntos</div>
-            <div>my nombre es javier</div>
-            <Button color={primary} variant="contained" size="small">soy frontend developer</Button>
+        <div className='grid grid-cols-1 bg-primario3'>
+            <div>
+                <div className="flex flex-col items-center">
+                    <h1 class="text-7xl text-secundario2 font-bold underline place-content-start mt-5 font-mono uppercase">
+                        Hello
+                    </h1>
+                <div className="text-secundario1 font-mono mt-5 text-4xl">aqui unos puntos</div>
+                </div>
+            </div>
+            <div>
+                <div className="flex justify-center items-center flex-col">
+                    
+                    <div className="text-4xl font-mono mb-5">my nombre es javier</div>
+                    <Boton nombre={'soy frontend developer'}/>
+                </div>
+            </div>
         </div>
     )
 }
