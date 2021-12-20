8c0ad02d38592f8281bab9d4a55dcbdc52682079
fimovol <fimovolpe@gmail.com>
2021-12-20 12:40:12 -0500

    agregando nuevos colores personalizados a mis componetes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primario1': '#101B94',
+      'primario2': '#4F5EFF',
+      'primario3': '#2E3CE0',
+      'secundario1': '#947501',
+      'secundario2': '#E0BB2F',
+    }
   },
   plugins: [],
 }
