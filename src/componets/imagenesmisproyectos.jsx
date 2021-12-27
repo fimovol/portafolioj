1e9419b3e2ae63c0577955e85141768f4ebed4f4
fimovol <fimovolpe@gmail.com>
2021-12-27 14:28:21 -0500

    cargar proyectos y mostralos en la pagina

--- src/componets/imagenesmisproyectos.jsx
+++ src/componets/imagenesmisproyectos.jsx
@@ -4,20 +4,24 @@ import luces from '../fotos/luces.jpg'
 import horario from '../fotos/horarios.jpg'
 import movil from '../fotos/movil.jpg'
 import gqldocker from '../fotos/gql.jpg'
-import api from '../fotos/api.jpg'
 
 
 export default function _(){
     const imagen='https://static-cdn.jtvnw.net/jtv_user_pictures/8d62f29a-375c-4ece-beb1-3567630d5d51-profile_image-300x300.png'
 
+    const calculadorahref = 'https://fimovol.github.io/calculadora/'
+    const luceshref = 'https://fimovol.github.io/lucesnavide-as/'
+    const horariohref = 'https://fimovol.github.io/tramoshorarios/'
+    const movilhref = 'https://github.com/fimovol/fpr_interfaces_senati'
+    const gqldockerhref = 'https://github.com/fimovol/test_gql_api_mongo'
+
     return(
         <div className='flex flex-wrap justify-center'>
-            <ProyectosQueRealice src={calculadora}/>
-            <ProyectosQueRealice src={luces}/>
-            <ProyectosQueRealice src={horario}/>
-            <ProyectosQueRealice src={movil}/>
-            <ProyectosQueRealice src={gqldocker}/>
-            <ProyectosQueRealice src={api}/>
+            <ProyectosQueRealice src={calculadora} href={calculadorahref}/>
+            <ProyectosQueRealice src={luces} href={luceshref}/>
+            <ProyectosQueRealice src={horario} href={horariohref}/>
+            <ProyectosQueRealice src={movil} href={movilhref}/>
+            <ProyectosQueRealice src={gqldocker} href={gqldockerhref}/>
         </div>
     )
 }
--- src/componets/trabajoscontenedor.jsx
+++ src/componets/trabajoscontenedor.jsx
@@ -1,5 +1,7 @@
-export const ProyectosQueRealice = ({src}) =>{
+export const ProyectosQueRealice = ({src,href}) =>{
     return <div className='w-48 h-48 mx-4 my-4'>
-                <img className='w-full h-full rounded hover:shadow-lg hover:shadow-secundario2 border-secundario1 hover:border-x-4 hover:border-y-4 transition-all' src={src}/>
+                <a href={href} target="_blank">
+                    <img className='w-full h-full rounded hover:shadow-lg hover:shadow-secundario2 border-secundario1 hover:border-x-4 hover:border-y-4 transition-all' src={src}/>
+                </a>
             </div>
 }
