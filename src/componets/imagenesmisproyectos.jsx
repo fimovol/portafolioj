6ceef8d88ae1ba3cae720eaa1d39e79118c2d11a
fimovol <fimovolpe@gmail.com>
2021-12-27 13:38:08 -0500

    guardando fotos de mis proyectos

--- src/componets/imagenesmisproyectos.jsx
+++ src/componets/imagenesmisproyectos.jsx
@@ -1,15 +1,23 @@
 import { ProyectosQueRealice } from './trabajoscontenedor'
+import calculadora from '../fotos/calculadora.jpg'
+import luces from '../fotos/luces.jpg'
+import horario from '../fotos/horarios.jpg'
+import movil from '../fotos/movil.jpg'
+import gqldocker from '../fotos/gql.jpg'
+import api from '../fotos/api.jpg'
+
 
 export default function _(){
+    const imagen='https://static-cdn.jtvnw.net/jtv_user_pictures/8d62f29a-375c-4ece-beb1-3567630d5d51-profile_image-300x300.png'
+
     return(
         <div className='flex flex-wrap justify-center'>
-            <ProyectosQueRealice/>
-            <ProyectosQueRealice/>
-            <ProyectosQueRealice/>
-            <ProyectosQueRealice/>
-            <ProyectosQueRealice/>
-            <ProyectosQueRealice/>
-            
+            <ProyectosQueRealice src={calculadora}/>
+            <ProyectosQueRealice src={luces}/>
+            <ProyectosQueRealice src={horario}/>
+            <ProyectosQueRealice src={movil}/>
+            <ProyectosQueRealice src={gqldocker}/>
+            <ProyectosQueRealice src={api}/>
         </div>
     )
 }
--- src/componets/trabajoscontenedor.jsx
+++ src/componets/trabajoscontenedor.jsx
@@ -1,5 +1,5 @@
-export const ProyectosQueRealice = () =>{
+export const ProyectosQueRealice = ({src}) =>{
     return <div className='w-48 h-48 mx-4 my-4'>
-                <img className='w-full h-full rounded hover:shadow-lg hover:shadow-secundario2 border-secundario1 hover:border-x-4 hover:border-y-4 transition-all' src='https://static-cdn.jtvnw.net/jtv_user_pictures/8d62f29a-375c-4ece-beb1-3567630d5d51-profile_image-300x300.png'/>
+                <img className='w-full h-full rounded hover:shadow-lg hover:shadow-secundario2 border-secundario1 hover:border-x-4 hover:border-y-4 transition-all' src={src}/>
             </div>
 }
