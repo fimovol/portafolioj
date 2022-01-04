11215da83ccadfb8ae2e7ca48aca4d7f8130bb72
fimovol <fimovolpe@gmail.com>
2022-01-04 10:18:39 -0500

    creando el blog por primera vez

---
+++ src/componets/blog.jsx
@@ -0,0 +1,5 @@
+export default function _(){
+  return <div className="w-screen h-screen flex items-center justify-center text-center">
+    <h1 className="uppercase bg-primario2 text-secundario2 text-7xl underline">este sera mi proximo blog</h1>
+  </div>
+}
